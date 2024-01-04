c7dc2a8a41ab38675c0f721378a519a19769560b
NawafSwe <nawaf.jr9@hotmail.com>
2024-01-04 22:29:59 +0300

    feat: modify the models to fix relationship issues

--- orders/server/created_order.go
+++ orders/server/created_order.go
@@ -3,6 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+
+	"github.com/nawafswe/orders-service/orders/server/models"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/nawafswe/orders-service/orders/proto"
 )
@@ -10,19 +15,53 @@ import (
 func (s *Server) Create(ctx context.Context, in *pb.Order) (*pb.Order, error) {
 
 	fmt.Printf("OrderService was invoked with Create method, with ctx: %v, in:%v\n", ctx, in)
-	items := []*pb.OrderedItem{
-		{
-			ItemId:          1,
-			OrderedQuantity: 1,
-			Price:           45.00,
-			Sku:             "drink-coffee-321-s",
-		},
+	createdOrder := &models.Order{
+		CustomerId: in.CustomerId,
+		GrandTotal: in.GrandTotal,
+		Status:     "new",
 	}
-	return &pb.Order{
-		OrderId:    1,
-		CustomerId: 1,
-		GrandTotal: 45.00,
-		Items:      items,
-		Status:     "New",
-	}, nil
+	tx := s.DB.WithContext(ctx).Create(createdOrder)
+
+	if tx.Error != nil {
+		return nil, status.Errorf(codes.Internal, "failed to commit order, err: %v", tx.Error)
+	}
+	if tx.RowsAffected > 0 {
+		log.Printf("Created order ===> %v\n", createdOrder)
+		log.Printf("order id  ===> %v\n", createdOrder.OrderId)
+		// create items
+		createdItems := []*models.OrderedItem{}
+		for _, item := range in.Items {
+			createdItems = append(createdItems, &models.OrderedItem{
+				OrderedQuantity: item.OrderedQuantity,
+				Price:           item.Price,
+				Sku:             item.Sku,
+				OrderID:         uint(createdOrder.OrderId),
+			})
+		}
+		log.Printf("createdItems ===> %v \n", createdItems)
+		btx := s.DB.WithContext(ctx).CreateInBatches(&createdItems, 10)
+
+		if err := btx.Error; err != nil {
+			tx.Rollback()
+			return nil, status.Errorf(codes.Internal, "failed to create items of order, err: %v", err)
+		}
+		preparedItems := []*pb.OrderedItem{}
+		for _, item := range createdItems {
+			preparedItems = append(preparedItems, &pb.OrderedItem{
+				ItemId:          item.ItemId,
+				OrderedQuantity: item.OrderedQuantity,
+				Price:           item.Price,
+				Sku:             item.Sku,
+			})
+		}
+		return &pb.Order{
+			OrderId:    createdOrder.OrderId,
+			Items:      preparedItems,
+			GrandTotal: createdOrder.GrandTotal,
+			Status:     createdOrder.Status,
+			CustomerId: createdOrder.CustomerId,
+		}, nil
+	}
+	return nil, status.Error(codes.Internal, "could not complete the operation")
+
 }
--- orders/server/models/ordered_item.go
+++ orders/server/models/ordered_item.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type OrderedItem struct {
 	gorm.Model
-	ItemId          int64 `gorm:"primarykey"`
+	ItemId          int64 `gorm:"uniqueIndex;autoIncrement"`
 	OrderedQuantity int32
 	Sku             string
-	price           float64
+	Price           float64
 	OrderID         uint `gorm:"column:order_id"` // Foreign key to the Order model
 }
