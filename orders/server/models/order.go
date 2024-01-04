594b653924d9d4730e39624e8dd39a8892774dca
NawafSwe <nawaf.jr9@hotmail.com>
2024-01-04 20:23:39 +0300

    feat: comments removal

--- orders/server/models/order.go
+++ orders/server/models/order.go
@@ -8,19 +8,3 @@ type Order struct {
 	// CreatedAt time.Time `gorm:"autoCreateTime:false"`
 	// UpdatedAt time.Time `gorm:"autoUpdateTime:false"`
 }
-
-// message Order {
-
-//     int64 order_id = 1;
-//     int64 customer_id = 2;
-//     string status = 3;
-//     double grand_total = 4;
-//     repeated OrderedItem items = 5;
-
-// }
-
-// message OrderStatus {
-//     int64 order_id = 1;
-//     string status = 2;
-
-// }
