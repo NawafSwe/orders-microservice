92bb914cbb1bf72c96f1bc184fb028ed3f964f46
NawafSwe <nawaf.jr9@hotmail.com>
2024-01-11 16:23:22 +0300

    feat: implement order creation and pass context object from rpc calls down to the db operations

--- pkg/v1/interfaces.go
+++ pkg/v1/interfaces.go
@@ -1,16 +1,18 @@
 package v1
 
 import (
+	"context"
 	"github.com/nawafswe/orders-service/internal/models"
 )
 
 type OrderRepo interface {
-	Create(order models.Order) (models.Order, error)
-	UpdateOrderStatus(id int64, status string) (models.Order, error)
+	Create(ctx context.Context, order models.Order) (models.Order, error)
+	UpdateOrderStatus(ctx context.Context, id int64, status string) (models.Order, error)
 }
 
 type OrderUseCase interface {
-	PlaceOrder(order models.Order) (models.Order, error)
-	ApproveOrder(orderId int64) error
-	RejectOrder(orderId int64) error
+	PlaceOrder(ctx context.Context, order models.Order) (models.Order, error)
+	ApproveOrder(ctx context.Context, orderId int64) error
+	RejectOrder(ctx context.Context, orderId int64) error
+	UpdateOrderStatus(ctx context.Context, orderId int64, status string) (models.Order, error)
 }
--- pkg/v1/repository/order_gorm.go
+++ pkg/v1/repository/order_gorm.go
@@ -1,9 +1,13 @@
 package repo
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/nawafswe/orders-service/internal/models"
 	interfaces "github.com/nawafswe/orders-service/pkg/v1"
 	"gorm.io/gorm"
+	"log"
 )
 
 type OrderRepoImpl struct {
@@ -14,9 +18,31 @@ func New(d *gorm.DB) interfaces.OrderRepo {
 	return OrderRepoImpl{db: d}
 }
 
-func (r OrderRepoImpl) Create(order models.Order) (models.Order, error) {
-	return models.Order{}, nil
+func (r OrderRepoImpl) Create(ctx context.Context, order models.Order) (models.Order, error) {
+	tx := r.db.WithContext(ctx).Create(&order)
+
+	if tx.Error != nil {
+		return models.Order{}, fmt.Errorf("error occurred while creating a new order, err: %w", tx.Error)
+	}
+	if tx.RowsAffected == 0 {
+		return models.Order{}, errors.New("failed to create order for unknown error")
+	}
+	log.Printf("created order with details --> %v\n", order)
+	var createdOrder models.Order
+	tx.Scan(&createdOrder)
+	return createdOrder, nil
 }
-func (r OrderRepoImpl) UpdateOrderStatus(id int64, status string) (models.Order, error) {
-	return models.Order{}, nil
+func (r OrderRepoImpl) UpdateOrderStatus(ctx context.Context, id int64, status string) (models.Order, error) {
+
+	tx := r.db.WithContext(ctx).Where("id = ? ", id).Updates(&models.Order{Status: status})
+	if tx.Error != nil {
+		return models.Order{}, fmt.Errorf("error occurred while updating order status, err: %w", tx.Error)
+	}
+	if tx.RowsAffected == 0 {
+		return models.Order{}, fmt.Errorf("order with id %v not found", id)
+	}
+	var o models.Order
+	tx.Scan(&o)
+
+	return o, nil
 }
