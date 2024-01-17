df9e8266284c2bbcd0ab2ff3af2ed0ed1dc95203
NawafSwe <nawaf.jr9@hotmail.com>
2024-01-17 10:29:07 +0300

    chore: change order use case mocks to include publish order created event

--- pkg/v1/interfaces.go
+++ pkg/v1/interfaces.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"context"
 	"github.com/nawafswe/orders-service/internal/models"
+	pb "github.com/nawafswe/orders-service/proto"
 )
 
 type OrderRepo interface {
@@ -15,4 +16,6 @@ type OrderUseCase interface {
 	UpdateOrderStatus(ctx context.Context, orderId int64, status string) (models.Order, error)
 	HandleOrderApproval(ctx context.Context)
 	HandleOrderRejection(ctx context.Context)
+	PublishOrderStatusChanged(order models.Order)
+	PublishOrderCreatedEvent(order *pb.Order)
 }
