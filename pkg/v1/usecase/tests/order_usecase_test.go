d550627268df7de7a45891ddb8c4e6d61d9f002c
NawafSwe <nawaf.jr9@hotmail.com>
2024-01-19 21:45:57 +0300

    strong testing for placing order successfully and with failure test case

--- pkg/v1/usecase/tests/order_usecase_test.go
+++ pkg/v1/usecase/tests/order_usecase_test.go
@@ -2,28 +2,29 @@ package use_case_tests
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"github.com/nawafswe/orders-service/internal/models"
+	messagesMock "github.com/nawafswe/orders-service/mocks/github.com/nawafswe/orders-service/pkg/messaging"
 	ordersMock "github.com/nawafswe/orders-service/mocks/github.com/nawafswe/orders-service/pkg/v1"
+	useCases "github.com/nawafswe/orders-service/pkg/v1/usecase"
 	"github.com/stretchr/testify/mock"
+	"gorm.io/gorm"
+	"slices"
 	"testing"
-	"time"
 )
 
 func TestOrderUseCase(t *testing.T) {
 	tests := map[string]struct {
-		Description string
-		Before      func(orderMock *ordersMock.MockOrderUseCase, order *models.Order)
-		Assert      func(t *testing.T, mock *ordersMock.MockOrderUseCase, order *models.Order)
-		Data        *models.Order
+		Description    string
+		Input          models.Order
+		ExpectedErr    error
+		ExpectedResult models.Order
 	}{
 		"PlaceOrderSuccessfully": {
 			Description: "Should Place order successfully",
-			Before: func(orderMock *ordersMock.MockOrderUseCase, order *models.Order) {
-				orderMock.On("PlaceOrder", mock.Anything, *order).Return(*order, nil)
-				//orderMock.On("PublishOrderCreatedEvent", mock.Anything).Return(nil)
-			},
-			Data: &models.Order{
+			ExpectedErr: nil,
+			ExpectedResult: models.Order{
+				Model:      gorm.Model{ID: 1},
 				CustomerId: 1,
 				Status:     "New",
 				GrandTotal: 10,
@@ -36,45 +37,23 @@ func TestOrderUseCase(t *testing.T) {
 					},
 				},
 			},
-			Assert: func(t *testing.T, orderMock *ordersMock.MockOrderUseCase, order *models.Order) {
-				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-				defer cancel()
-				// adding wait groups, to synchronize both go routines, to assert that order created event is published
-				//var wg sync.WaitGroup
-				//wg.Add(2)
-
-				//go func() {
-				//defer wg.Done()
-				o, err := orderMock.PlaceOrder(ctx, *order)
-				if err != nil {
-					t.Errorf("failed place order, expected error with nil but got %v\n", err)
-				}
-				if len(o.Items) != 1 {
-					t.Errorf("expected orded items 1 but got %v\n", len(o.Items))
-				}
-				if o.GrandTotal != order.GrandTotal {
-					t.Errorf("expected grand total of %v, but got %v", order.GrandTotal, o.GrandTotal)
-				}
-
-				//}()
-
-				//go func() {
-				//	defer wg.Done()
-				//	time.Sleep(10 * time.Second)
-				//}()
-
-				//wg.Wait()
-				orderMock.AssertCalled(t, "PlaceOrder", mock.Anything, *order)
-				//orderMock.AssertNumberOfCalls(t, "PublishOrderCreatedEvent", 1)
-				orderMock.AssertExpectations(t)
+			Input: models.Order{
+				CustomerId: 1,
+				Status:     "New",
+				GrandTotal: 10,
+				Items: []models.OrderedItem{
+					{
+						OrderedItemId:   1,
+						OrderedQuantity: 10,
+						Price:           1,
+						Sku:             "12su",
+					},
+				},
 			},
 		},
 		"FailPlaceOrderDueToInvalidItemQuantity": {
 			Description: "Should fail place order due to invalid item quantities",
-			Before: func(orderMock *ordersMock.MockOrderUseCase, order *models.Order) {
-				orderMock.On("PlaceOrder", mock.Anything, *order).Return(models.Order{}, fmt.Errorf("supplied quantity for item with sku %v, should be greater than zero, received is %v", order.Items[0].Sku, order.Items[0].OrderedQuantity))
-			},
-			Data: &models.Order{
+			Input: models.Order{
 				CustomerId: 1,
 				Status:     "New",
 				GrandTotal: 10,
@@ -87,25 +66,44 @@ func TestOrderUseCase(t *testing.T) {
 					},
 				},
 			},
-			Assert: func(t *testing.T, orderMock *ordersMock.MockOrderUseCase, order *models.Order) {
-				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-				defer cancel()
-				_, err := orderMock.PlaceOrder(ctx, *order)
-				if err == nil {
-					t.Errorf("failed place order, expected error with %v but got nil\n", err)
-				}
-				orderMock.AssertCalled(t, "PlaceOrder", mock.Anything, *order)
-				orderMock.AssertExpectations(t)
-			},
+			ExpectedResult: models.Order{},
+			ExpectedErr:    errors.New("supplied quantity for item with sku 12su, should be greater than zero, received is -10"),
 		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			orderUseCaseMock := ordersMock.NewMockOrderUseCase(t)
 			t.Logf("running %s", name)
-			test.Before(orderUseCaseMock, test.Data)
-			test.Assert(t, orderUseCaseMock, test.Data)
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			pubSubMock := messagesMock.NewMockMessageService(t)
+			ordersRepoMock := ordersMock.NewMockOrderRepo(t)
+			ordersUseCase := useCases.NewOrderUseCase(ordersRepoMock, pubSubMock)
+			// setting up mocks
+			// Assert mocks
+
+			if test.ExpectedErr == nil {
+				newOrder := test.Input
+				newOrder.ID = 1
+				ordersRepoMock.On("Create", mock.Anything, test.Input).Return(newOrder, nil)
+				pubSubMock.On("Publish", mock.Anything, "orderCreated", mock.Anything).Return(nil)
+				pubSubMock.On("Publish", mock.Anything, "orderStatusChanged", mock.Anything).Return(nil)
+			}
+
+			result, err := ordersUseCase.PlaceOrder(ctx, test.Input)
+			if err == nil && test.ExpectedErr != nil {
+				t.Errorf("expected error from %s is %v but got %v", name, test.ExpectedErr, err)
+			}
+			if result.ID != test.ExpectedResult.ID {
+				t.Errorf("expected order id is %v, but got %v", test.ExpectedResult.ID, result.ID)
+			}
+			if !slices.Equal(result.Items, test.ExpectedResult.Items) {
+				t.Errorf("created items not matched, expected is %v, but got %v", test.ExpectedResult.Items, result.Items)
+			}
+			if err == nil {
+				ordersRepoMock.AssertExpectations(t)
+				pubSubMock.AssertExpectations(t)
+			}
 		})
 	}
 }
