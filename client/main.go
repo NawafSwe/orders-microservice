77bcfc81b08a795d2f36acb0516f3d9f5b0bc64b
NawafSwe <nawaf.jr9@hotmail.com>
2024-01-22 21:19:24 +0300

    chore make tls optional

--- client/main.go
+++ client/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	pb "github.com/nawafswe/orders-service/proto"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
@@ -15,13 +18,18 @@ func main() {
 		log.Fatalf("failed to load .env file")
 	}
 
-	cred, err := credentials.NewClientTLSFromFile("ssl/server.crt", "")
-
-	if err != nil {
-		log.Fatalf("failed to obtain credentials, err: %v\n", err)
+	tlsEnabled := os.Getenv("TLS_ENABLED")
+	log.Printf("tlsEnabled: %v\n", tlsEnabled)
+	var copts []grpc.DialOption
+	if b, _ := strconv.ParseBool(tlsEnabled); b {
+		cred, err := credentials.NewClientTLSFromFile("ssl/server.crt", "")
+		copts = append(copts, grpc.WithTransportCredentials(cred))
+		if err != nil {
+			log.Fatalf("failed to obtain credentials, err: %v\n", err)
+		}
+	} else {
+		copts = append(copts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	}
-	copts := []grpc.DialOption{}
-	copts = append(copts, grpc.WithTransportCredentials(cred))
 	addr := "localhost:9000"
 	conn, err := grpc.Dial(addr, copts...)
 	if err != nil {
