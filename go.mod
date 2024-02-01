9695e9dda49de50b516a6e1c0d2114b786b8e95e
NawafSwe <nawaf.jr9@hotmail.com>
2024-02-01 16:02:01 +0300

    feat: implement logger

--- go.mod
+++ go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,18 +1,50 @@
 package logger
 
-import "log"
+import (
+	"github.com/sirupsen/logrus"
+	"io"
+	"log"
+	"os"
+)
 
 type Logger interface {
-	Log(data any)
+	Debug(data map[string]any, args any)
+	Info(data map[string]any, args any)
+	Warn(data map[string]any, args any)
+	Error(data map[string]any, args any)
 }
 
+// getConfiguredLogger
+// function to config the logger
+func getConfiguredLogger() *logrus.Logger {
+	l := logrus.New()
+	l.SetFormatter(&logrus.JSONFormatter{})
+	f, err := os.OpenFile("app.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatalf("failed to create a log file, err: %v", err)
+	}
+	// writing to log file and to datadog which is automatically will take what is written to os.Stdout
+	l.SetOutput(io.MultiWriter(os.Stdout, f))
+	l.SetLevel(logrus.DebugLevel)
+	return l
+}
 func NewLogger() Logger {
-	return LoggingImpl{}
+	return LoggingImpl{logger: getConfiguredLogger()}
 }
 
 type LoggingImpl struct {
+	logger *logrus.Logger
 }
 
-func (l LoggingImpl) Log(data any) {
-	log.Printf("%v\n", data)
+func (l LoggingImpl) Info(data map[string]any, args any) {
+	l.logger.WithFields(data).Info(args)
+}
+func (l LoggingImpl) Warn(data map[string]any, args any) {
+	l.logger.WithFields(data).Warn(args)
+}
+func (l LoggingImpl) Debug(data map[string]any, args any) {
+	l.logger.WithFields(data).Debug(args)
+}
+func (l LoggingImpl) Error(data map[string]any, args any) {
+	l.logger.WithFields(data).Error(args)
 }
